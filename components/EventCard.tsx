aa404c4e5f7a42c95344a45160e753e5ba811246
Harris-PS <psharris2002@gmail.com>
2026-01-28 12:57:28 +0530

    Implement PostHog

--- components/EventCard.tsx
+++ components/EventCard.tsx
@@ -1,36 +1,47 @@
+'use client'
 import Image from "next/image"
 import Link from "next/link"
+import posthog from "posthog-js"
 
 interface Props {
-    title: string
-    image: string
-    slug: string
-    location: string
-    date: string
-    time: string
+  title: string
+  image: string
+  slug: string
+  location: string
+  date: string
+  time: string
 }
 
 const EventCard = ({ title, image, slug, location, date, time }: Props) => {
+  const handleClick = () => {
+    posthog.capture('event_card_clicked', {
+      event_title: title,
+      event_slug: slug,
+      event_location: location,
+      event_date: date,
+    });
+  };
+
   return (
-    <Link href={`/events`} id="event-card">
-        <Image src={image} alt={title} width={410} height={300} className="poster" />
+    <Link href={`/events`} id="event-card" onClick={handleClick}>
+      <Image src={image} alt={title} width={410} height={300} className="poster" />
 
-        <div className="felx flex-row gap-2">
-          <Image src="icons/pin.svg" alt="location" width={14} height={14} />
-          <p>{location}</p>
+      <div className="flex flex-row gap-2">
+        <Image src="icons/pin.svg" alt="location" width={14} height={14} />
+        <p>{location}</p>
+      </div>
+
+      <h3 className="title">{title}</h3>
+
+
+      <div className="datetime">
+        <div><Image src="icons/calendar.svg" alt="date" width={14} height={14} />
+          <p>{date}</p>
         </div>
-
-        <h3 className="title">{title}</h3>
-
-
-        <div className="datetime">
-          <div><Image src="icons/calendar.svg" alt="date" width={14} height={14} />
-              <p>{date}</p>
-          </div>
-          <div><Image src="icons/clock.svg" alt="time" width={14} height={14} />
-              <p>{time}</p>
-          </div>
+        <div><Image src="icons/clock.svg" alt="time" width={14} height={14} />
+          <p>{time}</p>
         </div>
+      </div>
     </Link>
   )
 }
